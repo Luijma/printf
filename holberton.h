7aa226416cb16a4a54c4f42bbe323cc35ac0c9a2
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-07-24 22:21:25 +0000

    Created header files, Created function files

---
+++ holberton.h
@@ -0,0 +1,11 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void _print_string(char *str, int *n);
+void _print_char(char c, int *n);
+int _putchar(char c);
+
+#endif /* _HOLBERTON_H_ */
