583f8eae82680ff59ac0fb1700d919b2eaa42eb1
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-07-24 23:04:33 +0000

    prototypes for printf and printnum

--- holberton.h
+++ holberton.h
@@ -6,6 +6,8 @@
 
 void _print_string(char *str, int *n);
 void _print_char(char c, int *n);
+void _print_num(int num, int *n);
+void _printf(char *format, ...);
 int _putchar(char c);
 
 #endif /* _HOLBERTON_H_ */
