b4cf00028adf9b7f07addd4ad8f42072e9264326
Luijma <rsluismanuel@gmail.com>
2021-07-24 23:33:06 +0000

    Finished test files, fixed holberton.h prototypes for _printf

--- 0-main.c
+++ 0-main.c
@@ -5,8 +5,12 @@
  */
 int main (void)
 {
+	int n;
 	_printf("Simple Sentence\n");
-	_printf("Not so simple %s", "sentence");
-
+	_printf("Not so simple %s\n", "sentence");
+	n = _printf("12345\n");
+	printf("%d\n", n);
+	n = _printf("123%c\n", '4');
+	printf("%d\n", n);
 	return (0);
 }
