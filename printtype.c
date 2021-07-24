6e3819d5b8840ae3f7a893769199b7c1f303ed84
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-07-24 23:20:48 +0000

    adding printnum

--- printtype.c
+++ printtype.c
@@ -19,10 +19,31 @@ void _print_string(char *str, int *n)
 /**
  * print_char - print char passed.
  * @c: character passed.
- * Return: nothing
  */
 void _print_char(char c, int *n)
 {
 	_putchar(c);
 	n++;
+}
+
+/**
+ * print_num - print number.
+ * @num: number passed.
+ * @n: pointer
+ */
+void _print_num(int num, int *n)
+{
+	if (num < 0)
+	{
+		_print_char('_', &n);
+	}
+	if (num == 0)
+	{
+		_print_char(num + '0', &n);
+	}
+	if (num / 10)
+	{
+		_print_num(num, &n);
+	}
+	_print_char(num % 10, &n);
 }
