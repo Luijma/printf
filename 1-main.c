cfca3052065655c0d939a21cce1d51edf222f8d6
Luijma <rsluismanuel@gmail.com>
2021-07-27 23:40:14 +0000

    Fixing negative issue on Minimum number

--- 1-main.c
+++ 1-main.c
@@ -13,5 +13,11 @@ int main (void)
     printf("Length:[%d, %i]\n", len2, len2);
     _printf("Negative:[%d]\n", -762534);
     printf("Negative:[%d]\n", -762534);
+	len = _printf("Printing numbers: %d\n", -1000);
+	_printf("_printf MAX:%d \n", INT_MAX);
+	printf("printf MAX:%d \n", INT_MAX);
+	_printf("_printf MIN:%d \n", INT_MIN);
+	printf("printf MIN:%d \n", INT_MIN);
+	
 	return (0);
 }
